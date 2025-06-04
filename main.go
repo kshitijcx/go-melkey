c9133f134071aa7cbd0eb631b10aa0396185198d
kshitijcx <kshitij.cx@gmail.com>
2025-06-04 22:35:52 +0530

    add conditionals

--- main.go
+++ main.go
@@ -1,67 +1,28 @@
 package main
 
-import (
-	"fmt"
-	"testing/iotest"
-)
+import "fmt"
 
 func main() {
-	// fmt.Println("hello")
-	//variables
-	var name string = "kshitij"
-	fmt.Printf("my name: %s %d\n", name, 1000)
-	age := 22
-	fmt.Printf("%d\n", age)
-	var city string
-	city = "Dharamshala"
-	fmt.Printf("%s ", city)
-	var country, continent string = "India", "Asia"
-	fmt.Println(country, continent)
-	var (
-		isEmployed bool   = true
-		salary     int    = 20000
-		position   string = "intern"
-	)
-	fmt.Println(isEmployed, salary, position)
+	//conditionals
+	age := 30
+	if age >= 18 {
+		fmt.Println("you are an adult")
+	} else if age >= 13 {
+		fmt.Println("you are a teenager")
+	} else {
+		fmt.Println("you are a child")
+	}
 
-	//default values
-	var num int
-	var st string
-	var fl float64 //%f
-	var bo bool    //%t
-	fmt.Println(num, st, fl, bo)
+	day:="Thursday"
 
-	//constants
-	const pi = 3.14       //can be unused
-	const number int = 32 //can be types and untyped
-	const (
-		mon  = 1
-		tues = 2
-		wed  = 3
-	)
-
-	//enum - no inbuilt enum just a workaround
-	const (
-		Jan int = iota + 1
-		// Jan = iota + 1
-		Feb
-		Mar
-		Apr
-	)
-
-	//functions
-	//add->private (cant be exported), Add->public(can be exported)
-	fmt.Println(add(2, 3))
-	sum, prod := sumandprod(2, 6) //sum,_ := ... to ommit other value
-	fmt.Println(sum, prod)
-}
-
-// single returns
-func add(num1 int, num2 int) int {
-	return num1 + num2
-}
-
-// multiple returns
-func sumandprod(a, b int) (int, int) {
-	return a + b, a * b
+	switch day {
+		case "Monday":
+			fmt.Println("First day")
+		case "Tuesday","Wednesday","Thursday":
+			fmt.Println("Mid week")
+		case "Friday":
+			fmt.Println("TGIF")
+		default:
+			fmt.Println("Weekend")
+	}
 }
